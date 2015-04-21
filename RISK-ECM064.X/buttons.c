11292f9239d58e52a814224cda6e3f1c88bc0c84
Jacob Varnau <jacobvarnau@gmail.com>
2015-04-21 14:36:22 -0400

    added power switch to buttons

--- RISK-ECM064.X/buttons.c
+++ RISK-ECM064.X/buttons.c
@@ -13,11 +13,14 @@ int advanceFlag = READY;
 int cancelFlag = READY;
 int nextFlag = READY;
 int previousFlag = READY;
+int powerOffFlag = READY;
+int powerOnFlag = READY;
 
 static unsigned buttonAdvanceState = 0;
 static unsigned buttonCancelState = 0;
 static unsigned encoderAState = 0;
 static unsigned encoderBState = 0;
+static unsigned buttonPowerState = 0;
 
 static unsigned previousEncoderA = 0;
 static unsigned previousEncoderB = 0;
@@ -30,22 +33,36 @@ void __ISR(_TIMER_2_VECTOR, IPL2SRS) pollButtons()
     buttonCancelState = (buttonCancelState << 1) | (buttonStates & BUTTON_CANCEL_MASK);
     encoderAState = (encoderAState << 1) | (buttonStates & ENCODER_A_MASK);
     encoderBState = (encoderBState << 1) | (buttonStates & ENCODER_B_MASK);
+    buttonPowerState = (buttonPowerState << 1) | (buttonStates & BUTTON_POWER_MASK);
 
     // Button has been pressed if the last six reads were true
     // Button has been released if the last six reads were false
-    if(advanceFlag == READY && (buttonAdvanceState & BUTTON_ADVANCE_MASK) == 0)
+    if(advanceFlag == READY && (buttonAdvanceState & BUTTON_ADVANCE_STATE_MASK) == 0)
         advanceFlag = SET;
-    else if((buttonAdvanceState & BUTTON_ADVANCE_MASK) == BUTTON_ADVANCE_MASK)
+    else if((buttonAdvanceState & BUTTON_ADVANCE_STATE_MASK) == BUTTON_ADVANCE_STATE_MASK)
         advanceFlag = READY;
 
-    if(cancelFlag == READY && (buttonCancelState & BUTTON_CANCEL_MASK) == 0)
+    if(cancelFlag == READY && (buttonCancelState & BUTTON_CANCEL_STATE_MASK) == 0)
         cancelFlag = SET;
-    else if((buttonCancelState & BUTTON_CANCEL_MASK) == BUTTON_CANCEL_MASK)
+    else if((buttonCancelState & BUTTON_CANCEL_STATE_MASK) == BUTTON_CANCEL_STATE_MASK)
         cancelFlag = READY;
 
-    if((encoderAState & ENCODER_A_MASK) == 0)
+    if((buttonPowerState & BUTTON_POWER_STATE_MASK) == 0)
+    {
+        if(powerOffFlag == READY)
+            powerOffFlag = SET;
+        powerOnFlag = READY;
+    }
+    else if((buttonPowerState & BUTTON_POWER_STATE_MASK) == BUTTON_POWER_STATE_MASK)
+    {
+        if(powerOnFlag == READY)
+            powerOnFlag = SET;
+        powerOffFlag = READY;
+    }
+
+    if((encoderAState & ENCODER_A_STATE_MASK) == 0)
         previousEncoderA = 0;
-    else if((encoderAState & ENCODER_A_MASK) == ENCODER_A_MASK)
+    else if((encoderAState & ENCODER_A_STATE_MASK) == ENCODER_A_STATE_MASK)
     {
         if(previousEncoderA == 0)
         {
@@ -57,9 +74,9 @@ void __ISR(_TIMER_2_VECTOR, IPL2SRS) pollButtons()
         previousEncoderA = 1;
     }
 
-    if((encoderBState & ENCODER_B_MASK) == 0)
+    if((encoderBState & ENCODER_B_STATE_MASK) == 0)
         previousEncoderB = 0;
-    else if((encoderBState & ENCODER_B_MASK) == ENCODER_B_MASK)
+    else if((encoderBState & ENCODER_B_STATE_MASK) == ENCODER_B_STATE_MASK)
         previousEncoderB = 1;
 
     IFS0bits.T2IF = 0; // clear interrupt flag
--- RISK-ECM064.X/buttons.h
+++ RISK-ECM064.X/buttons.h
@@ -14,35 +14,45 @@
 #define BUTTON_CANCEL_PIN   PORTFbits.RF5
 #define ENCODER_PIN_A       PORTFbits.RF0
 #define ENCODER_PIN_B       PORTFbits.RF1
+#define BUTTON_POWER_PIN    PORTFbits.RF3
 
 #define BUTTON_ADVANCE_SHIFT 4
 #define BUTTON_CANCEL_SHIFT 5
 #define ENCODER_A_SHIFT 0
 #define ENCODER_B_SHIFT 1
+#define BUTTON_POWER_SHIFT 3
 
 #define BUTTON_ADVANCE_MASK 0x00000010
 #define BUTTON_CANCEL_MASK 0x00000020
 #define ENCODER_A_MASK 0x00000001
 #define ENCODER_B_MASK 0x00000002
+#define BUTTON_POWER_MASK 0x0000008
 
 #define BUTTON_ADVANCE_STATE_MASK 0x000003F0
 #define BUTTON_CANCEL_STATE_MASK 0x000007E0
 #define ENCODER_A_STATE_MASK 0x0000003F
 #define ENCODER_B_STATE_MASK 0x0000007E
+#define BUTTON_POWER_STATE_MASK 0x000001F8
 
 extern int advanceFlag;
 extern int cancelFlag;
 extern int nextFlag;
 extern int previousFlag;
+extern int powerOffFlag;
+extern int powerOnFlag;
 
 static inline int flagSetAdvance() { return advanceFlag == 1; }
 static inline int flagSetCancel() { return cancelFlag == 1; }
 static inline int flagSetNext() { return nextFlag == 1; }
 static inline int flagSetPrevious() { return previousFlag == 1; }
+static inline int flagSetPowerOff() { return powerOffFlag == 1; }
+static inline int flagSetPowerOn() { return powerOnFlag == 1; }
 
 static inline void clearFlagAdvance() { advanceFlag = -1; }
 static inline void clearFlagCancel() { cancelFlag = -1; }
 static inline void clearFlagNext() { nextFlag = -1; }
 static inline void clearFlagPrevious() { previousFlag = -1; }
+static inline void clearFlagPowerOff() { powerOffFlag = -1; }
+static inline void clearFlagPowerOn() { powerOnFlag = -1; }
 
 #endif	/* BUTTONS_H */
